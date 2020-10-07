c407c0e4b4dda17e0628d778f5648acc74e5d617
louisgirard <louis.girard17@hotmail.com>
2020-10-07 14:23:08 +0200

    Added click-to-move system

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -1,10 +1,14 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UIElements;
 
 public class Mover : MonoBehaviour
 {
     [SerializeField] Transform target;
     NavMeshAgent navMeshAgent;
+
+    Ray ray;
 
     // Start is called before the first frame update
     void Start()
@@ -14,7 +18,20 @@ public class Mover : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {        
+        // Move to click
+        if(Input.GetMouseButtonDown(0))
+        {
+            MoveToCursor();
+        }
+    }
+
+    private void MoveToCursor()
     {
-        navMeshAgent.SetDestination(target.position);
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit hit))
+        {
+            navMeshAgent.SetDestination(hit.point);
+        }
     }
 }
