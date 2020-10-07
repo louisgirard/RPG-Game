21775f173dd8e780e1654f0834d8a83366349cd4
louisgirard <louis.girard17@hotmail.com>
2020-10-07 14:32:46 +0200

    Added camera follow player

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    [SerializeField] Transform player;
+
+    Vector3 cameraOffset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraOffset = transform.position - player.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.position + cameraOffset;
+    }
+}
