ca91c1018d6a4a10547c3ccce517b64b664a532d
louisgirard <louis.girard17@hotmail.com>
2020-10-09 15:07:48 +0200

    Added raycast for combat

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -1,19 +1,44 @@
 ﻿using UnityEngine;
 using RPG.Movement;
+using RPG.Combat;
 
 namespace RPG.Control
 {    public class PlayerController : MonoBehaviour
     {
         Mover mover;
+        Fighter fighter;
 
         // Start is called before the first frame update
         void Start()
         {
             mover = GetComponent<Mover>();
+            fighter = GetComponent<Fighter>();
         }
 
         // Update is called once per frame
         void Update()
+        {
+            InteractWithCombat();
+            InteractWithMovement();
+        }
+
+        private void InteractWithCombat()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
+                foreach (RaycastHit hit in hits)
+                {
+                    CombatTarget target = hit.transform.GetComponent<CombatTarget>();
+                    if (target != null)
+                    {
+                        fighter.Attack(target);
+                    }
+                }
+            }
+        }
+
+        private void InteractWithMovement()
         {
             // Move to click
             if (Input.GetMouseButton(0))
@@ -24,11 +49,15 @@ namespace RPG.Control
 
         private void MoveToCursor()
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out RaycastHit hit))
+            if (Physics.Raycast(GetMouseRay(), out RaycastHit hit))
             {
                 mover.MoveTo(hit.point);
             }
+        }
+
+        private static Ray GetMouseRay()
+        {
+            return Camera.main.ScreenPointToRay(Input.mousePosition);
         }
     }
 }
