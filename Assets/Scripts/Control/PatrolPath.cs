ac6ed7b4bad3ece3b68ba0178b17a10dc3853563
louisgirard <louis.girard17@hotmail.com>
2020-10-31 17:48:15 +0100

    Added patrol path

---
+++ Assets/Scripts/Control/PatrolPath.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace RPG.Control
+{
+    public class PatrolPath : MonoBehaviour
+    {
+        private void OnDrawGizmos()
+        {
+            for(int i = 0; i < transform.childCount; i++)
+            {
+                int j = (i + 1) % transform.childCount;
+                Gizmos.DrawSphere(GetWaypoint(i), 0.2f);
+                Gizmos.DrawLine(GetWaypoint(i), GetWaypoint(j));
+            }
+        }
+
+        private Vector3 GetWaypoint(int i)
+        {
+            return transform.GetChild(i).position;
+        }
+    }
+}
