d4f2088c9187c7a42decee8b0b4cf76ac015a79e
louisgirard <louis.girard17@hotmail.com>
2020-10-31 13:14:32 +0100

    Added enemy chase distance

---
+++ Assets/Scripts/Control/AIController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace RPG.Control
+{
+    public class AIController : MonoBehaviour
+    {
+        [SerializeField] float chaseDistance = 5f;
+        Transform target;
+
+        private void Start()
+        {
+            target = GameObject.FindGameObjectWithTag("Player").transform;    
+        }
+
+        private void Update()
+        {
+            if (DistanceToTarget() <= chaseDistance)
+            {
+                print(name + " Should chase");
+            }
+        }
+
+        private float DistanceToTarget()
+        {
+            return Vector3.Distance(transform.position, target.position);
+        }
+    }
+}
