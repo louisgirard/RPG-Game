0591e1797b651fd13c46e6380103a89ed4170457
louisgirard <louis.girard17@hotmail.com>
2020-10-31 17:28:31 +0100

    Added enemies suspicious behaviour

--- Assets/Scripts/Control/AIController.cs
+++ Assets/Scripts/Control/AIController.cs
@@ -8,16 +8,20 @@ namespace RPG.Control
     public class AIController : MonoBehaviour
     {
         [SerializeField] float chaseDistance = 5f;
+        [SerializeField] float suspiciousTime = 3f;
         Transform target;
+        ActionScheduler actionScheduler;
         Fighter fighter;
         Mover mover;
         Health health;
 
         Vector3 guardLocation;
+        float timeSinceLastSawPlayer = Mathf.Infinity;
 
         private void Start()
         {
             target = GameObject.FindGameObjectWithTag("Player").transform;
+            actionScheduler = GetComponent<ActionScheduler>();
             fighter = GetComponent<Fighter>();
             mover = GetComponent<Mover>();
             health = GetComponent<Health>();
@@ -30,12 +34,20 @@ namespace RPG.Control
 
             if (InAttackRange() && fighter.CanAttack(target.gameObject))
             {
+                timeSinceLastSawPlayer = 0;
                 fighter.Attack(target.gameObject);
+            }
+            else if (timeSinceLastSawPlayer < suspiciousTime)
+            {
+                //Suspicious state, just wait
+                actionScheduler.CancelAction();
             }
             else
             {
                 mover.StartMoveAction(guardLocation);
             }
+
+            timeSinceLastSawPlayer += Time.deltaTime;
         }
 
         private bool InAttackRange()
